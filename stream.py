2e26dd6cd9ddc0cc9c1052c6f41d8948ebc02515
Pro <aditya.gajbhiye.20@gmail.com>
2016-03-15 02:24:53 +0530

    completed

--- stream.py
+++ stream.py
@@ -27,63 +27,46 @@ start_time = time.time()
 # Get a sample of the public data following through Twitter
 iterator = twitter_stream.statuses.filter(track=str(sys.argv[1]), language="en")
 
-# Print each tweet in the stream to the screen 
-# Here we set it to stop after getting 1000 tweets. 
-# You don't have to set it to stop, but can continue running 
-# the Twitter API to collect data for days or even longer. 
-tweet_count = 380
+# main
+#tweet_count = 2000
 cache = []
 cache_score = {}
+count = 1
 s = time.time()
-for tweet in iterator:
-    tweet_count -= 1
-    # Twitter Python Tool wraps the data returned by Twitter 
-    # as a TwitterDictResponse object.
-    # We convert it back to the JSON format to print/score
-    # ob = json.loads(json.dumps(tweet).strip())
-    try:
-        line = unicodedata.normalize('NFKD', tweet['text']).encode('ascii','ignore')
-        line = line.split(' ')
-        for i in line:
-            if i in cache:
-                cache_score[i] = [cache_score[i][0] + 1, time.time()]
-            else:
-                cache.append(i)
-                cache_score[i] = [ 1 ,time.time()]
-        #print cache
-        #print cache_score
-        print len(cache)
+extra_time = 0
+try:
+    for tweet in iterator:
+        #tweet_count -= 1
+        try:
+            line = unicodedata.normalize('NFKD', tweet['text']).encode('ascii','ignore')
+            line = line.split(' ')
+            extra_s = time.time()
+            for i in line:
+                if i in cache:
+                    cache_score[i] = [cache_score[i][0] + 1, time.time()]
+                else:
+                    cache.append(i)
+                    cache_score[i] = [ 1 ,time.time()]
+            extra_time += time.time() - extra_s
+            print len(cache)
 
-    except UnicodeEncodeError:
-        pass
-    t = time.time()
-    print t - s
-    if t - s > 40:
-        print cache_score
-        for i in cache:
-            if  t - cache_score[i][1]> 30:
-                cache_score[i] = [cache_score[i][0] - 1, time.time()]
-                if cache_score[i][0] <= 0:
-                    del cache_score[i]
-                    cache.remove(i)
-        s = time.time()
-        print cache_score
-    '''if t - s > 60:
-        print cache_score
-        for i in cache:
-            if  t - cache_score[i][1]> 30:
-                cache_score[i] = [cache_score[i][0] - 1, time.time()]
-                if cache_score[i][0] <= 0:
-                    del cache_score[i]
-                    cache.remove(i)
-        s = time.time()
-        print cache_score
-'''
-    # The command below will do pretty printing for JSON data, try it out
-    # print json.dumps(tweet, indent=4)
-       
-    if tweet_count <= 0:
-        print "count over"
-        break 
-#f.close()
-print time.time() - start_time
+        except UnicodeEncodeError or KeyError:
+            pass
+        t = time.time()
+        print t - s, extra_time
+        if t - s > count*60:
+            print cache_score
+            for i in cache:
+                if  t - cache_score[i][1] > 30:
+                    cache_score[i] = [cache_score[i][0] - 1, time.time()]
+                    if cache_score[i][0] <= 0:
+                        del cache_score[i]
+                        cache.remove(i)
+            count += 1
+            print cache_score
+           
+        #if tweet_count <= 0:
+        #    print "count over"
+        #    break 
+except KeyboardInterrupt:
+    print time.time() - start_time
